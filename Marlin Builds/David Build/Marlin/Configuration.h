a75286abbb6fee4c6de1b7d663f381580a8e8b63
Forsyth-Creations <robert.h.forsyth@gmail.com>
2022-03-20 21:51:21 -0400

    New sdtuff

--- Marlin Builds/David Build/Marlin/Configuration.h
+++ Marlin Builds/David Build/Marlin/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Roi, My King"
+#define CUSTOM_MACHINE_NAME "Cosimo"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
